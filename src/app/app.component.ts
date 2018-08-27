a084f79135d49d7d9b2e5d361f14fb4231be5d86
alexandrovichAlexandrovich <danfeok@gmail.com>
2018-08-27 19:06:50 -0400

    CHANGED TO USE PARENT MODEL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Directive, Component, Output, EventEmitter} from '@angular/core';
 import {ListComponent} from './list/list.component';
 import index from '../../node_modules/@angular/cli/lib/cli';
+import {listItem} from './listItem';
 
 
 @Component({
@@ -11,10 +12,11 @@ import index from '../../node_modules/@angular/cli/lib/cli';
 export class AppComponent {
 
   lists: string[] = [];
+  contents: {[listName: string]: listItem[]} = {};
   badNewListName = false;
   warning = '';
 
-  searchFilter='';
+  searchFilter = '';
 
   addList (newListName) {
     if (newListName === '') {
@@ -24,6 +26,7 @@ export class AppComponent {
     } else {
       this.badNewListName = false;
       this.warn('');
+      this.contents[newListName] = [];
       this.lists.unshift(newListName);
     }
   }
@@ -35,24 +38,44 @@ export class AppComponent {
   removeList(listName: string) {
     const indexToRemove = this.lists.indexOf(listName);
     this.lists.splice(indexToRemove, 1);
+    delete this.contents[listName];
   }
 
   moveToFront(ev: string) {
-    let a = this.lists.splice(this.lists.indexOf(ev), 1)[0];
+    const a = this.lists.splice(this.lists.indexOf(ev), 1)[0];
     this.lists.unshift(a);
   }
 
   moveToTarget(ev: number[]) {
     const fromIndex = ev[0];
     const toIndex = ev[1] < this.lists.length ? ev[1] : this.lists.length - 1;
-    if (fromIndex === toIndex) return;
+    if (fromIndex === toIndex) { return; }
     const elem = this.lists.splice(fromIndex, 1)[0];
     // console.log(this.lists);
     this.lists.splice(toIndex, 0, elem);
     // console.log(this.lists);
   }
 
-  search(input: string){
+  search(input: string) {
     this.searchFilter = input;
+  }
+
+  addItemToList(ev: [string , string]) {
+    const listName = ev[0];
+    const newItem = {
+      text: ev[1]
+    };
+    this.contents[listName].push(newItem);
+  }
+
+  removeItemFromList(ev: [string, listItem]) {
+    console.log(ev[0]+' '+ev[1].text)
+    const i = this.contents[ev[0]].indexOf(ev[1]);
+    console.log(i);
+    const removed = this.contents[ev[0]].splice(i, 1);
+    console.log(removed[0].text);
+    for (let item of this.contents[ev[0]]) {
+      console.log('remaining: ' + item.text);
+    }
   }
 }
